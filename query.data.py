28ff0798d86b6612e34985fb388d49df76524830
drewdev02 <100895160+drewdev02@users.noreply.github.com>
2022-03-04 01:24:30 -0500

    Add files via upload

---
+++ query.data.py
@@ -0,0 +1,29 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+
+
+def start(update, context):
+    message = update.effective_message
+    message.reply_text('Click abajo para continuar', reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton(text="CLICK AQUI", callback_data="test")]]))
+
+
+def test(update, context):
+    message = update.effective_message
+    query = update.callback_query
+    query.answer()
+
+    if query.data == 'test':
+        message.reply_text(text='SI, lo admito soy fan del chocolate!!')
+
+
+if __name__ == '__main__':
+    updater = Updater(token='5143590496:AAHGff4zeoyanB9Fnvw33foLLFADD2toPPQ')
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CallbackQueryHandler(test, pattern='test'))
+    
+    updater.start_polling()
+    print('bot is started')
+    updater.idle()
