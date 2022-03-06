fc60dc98ea6549f9f0f969476a6b7527df0ee4e2
drewdev02 <100895160+drewdev02@users.noreply.github.com>
2022-03-06 00:53:14 -0500

    Add files via upload

--- query.data.py
+++ query.data.py
@@ -7,14 +7,22 @@ def start(update, context):
     message.reply_text('Click abajo para continuar', reply_markup=InlineKeyboardMarkup([
         [InlineKeyboardButton(text="CLICK AQUI", callback_data="test")]]))
 
+def help(Update, context):
+    message = update.effective_message
+    message.reply_text('Click abajo para continuar', reply_markup=InlineKeyboardMarkup([
+        [InlineKeyboardButton(text="Help me", callback_data="Help")]]))
+    
 
 def test(update, context):
     message = update.effective_message
     query = update.callback_query
     query.answer()
+    
 
     if query.data == 'test':
         message.reply_text(text='SI, lo admito soy fan del chocolate!!')
+
+
 
 
 if __name__ == '__main__':
