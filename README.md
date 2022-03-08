801617c638649bfe708c6b37b5e9e6a88e95bdc1
drewdev02 <100895160+drewdev02@users.noreply.github.com>
2022-03-08 15:30:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Telegram-bot
+Mi primer bot hecho en python xd
--- query.data.py
+++ query.data.py
@@ -1,21 +1,36 @@
 import os
 import qrcode
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, Filters,MessageHandler
-from telegram import ChatAction
+from telegram import ChatAction, InlineKeyboardMarkup, InlineKeyboardButton
 
 IMPUT_TEXT = 0
 
 def start(update, context):
-    update.message.reply_text('Hola bienvenido')
+    update.message.reply_text(
+        text='Hola bienvenido!!\n\nUsa "/qr" para generar un qr',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Generar qr', callback_data='qr')],
+           # [InlineKeyboardButton(text='Autor', url='@Assange')]
+        ])
+    )
 
 def help(update, context):
-    update.message.reply_text('Envia /qr para generar un QR')    
+    update.message.reply_text('Este bot fue creado por @Assange escribe para cualquier sugerencia')    
 
 def qr_command_handler(update, context):
-    update.message.reply_text('Envia un texto para generar un QR')
+    update.message.reply_text('Envia el texto para generar un QR')
     
     return IMPUT_TEXT
 
+def qr_callback_handler(update, context):
+    query = update.callback_query
+    query.answer()
+    
+    query.edit_message_text(
+        text='Envia el texto para generar un QR'
+    )
+     
+    return IMPUT_TEXT
 def generate_qr(text):
     
     filename = text + '.jpg'
@@ -49,14 +64,15 @@ def imput_text(update, context):
 
 if __name__ == '__main__':
     
-    updater = Updater(token='5157710415:AAFF7mqTrNzrH-a6JrOWCQQ5NN40JXs9zlM')
+    updater = Updater(token='5157710415:AAFF7mqTrNzrH-a6JrOWCQQ5NN40JXs9zlM', use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('help', help))
     dp.add_handler(ConversationHandler(
         entry_points=[
-           CommandHandler('qr', qr_command_handler)   
+           CommandHandler('qr', qr_command_handler),
+           CallbackQueryHandler(pattern='qr', callback =qr_callback_handler)   
             ],
          states={
          IMPUT_TEXT: [MessageHandler(Filters.text, imput_text)]
